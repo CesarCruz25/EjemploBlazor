4ee1e0635d94adb71370167c860ec6e227894677
CesarCruz25 <cruzcesarbanegas@gmail.com>
2022-04-26 17:16:57 -0600

    Examen III

---
+++ EjemploBlazor/Blazor/Controllers/LoginControllers.cs
@@ -0,0 +1,81 @@
+﻿using Blazor.Data;
+using Datos.Interfaces;
+using Datos.Repositorios;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+using Modelos;
+using System.Security.Claims;
+
+namespace Blazor.Controllers
+{
+    public class LoginControllers : Controller
+    {
+        private readonly MySQLConfiguration _configuration;
+        private IUsuarioRepositorio _usuarioRepositorio;
+
+        public LoginControllers(MySQLConfiguration configuration)
+        {
+            _configuration = configuration;
+            _usuarioRepositorio = new UsuarioRepositorio(configuration.CadenaConexion);
+        }
+
+        [HttpPost("/account/login")]
+        public async Task<IActionResult> Login(Login login)
+        {
+            string rol = string.Empty;
+            try
+            {
+                bool usuarioValido = await _usuarioRepositorio.ValidaUsuario(login);
+                if (usuarioValido)
+                {
+                    Usuario usu = await _usuarioRepositorio.GetPorCodigo(login.Codigo);
+                    if (usu.EstaActivo) 
+                    {
+                        rol = usu.Rol;
+                        var claims = new[]
+                        {
+                            new Claim(ClaimTypes.Name, usu.Codigo),
+                            new Claim(ClaimTypes.Role, rol)
+                        };
+
+                        var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+                        var claimsPrincipal = new ClaimsPrincipal(claimsIdentity);
+
+                        await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, claimsPrincipal,
+                                                     new AuthenticationProperties
+                                                     {
+                                                         IsPersistent = true,
+                                                         ExpiresUtc = DateTime.UtcNow.AddMinutes(20)
+                                                     });
+                    }
+                    else
+                    {
+                        return LocalRedirect("/login/Usuario Inactivo");
+                    }
+                }
+                else
+                {
+                    return LocalRedirect("/login/Datos de usuario invalido");
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+                return LocalRedirect("/login/Datos de usuario invalido");
+            }
+
+            return LocalRedirect("/");
+        }
+
+        [ HttpGet("/acount/logout")]
+
+        public async Task<IActionResult> Logout()
+        {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            return LocalRedirect("/");
+        }
+
+    }
+}
---
+++ EjemploBlazor/Modelos/Producto.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Modelos
+{
+    public class Producto
+    {
+        [Required(ErrorMessage = "El Campo Codigo es Obligatorio")]
+        public string Codigo { get; set; }
+        [Required(ErrorMessage = "El Campo Descripcion es Obligatorio")]
+        public string Descripcion { get; set; }
+        public decimal Precio { get; set; }
+        public int Existencia { get; set; }
+        public byte[] Imagen { get; set; }
+    }
+}
